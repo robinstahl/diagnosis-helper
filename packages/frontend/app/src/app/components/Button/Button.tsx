28ca923131321fc145b9303564cfd6e23c7ea8ac
robin <stahl.ro@icloud.com>
2024-06-24 23:22:42 +0200

    add hook, implement in frontend

--- packages/frontend/app/src/app/components/Button/Button.tsx
+++ packages/frontend/app/src/app/components/Button/Button.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import "./Button.css";
+import React from 'react';
+import './Button.css';
 
 interface Props {
   onClick: () => void;
-  styleType: "primary" | "secondary" | "danger";
+  styleType: 'primary' | 'secondary' | 'danger';
   text: string;
+  disabled?: boolean;
 }
 
-const Button: React.FC<Props> = ({ onClick, styleType, text }) => {
+const Button: React.FC<Props> = ({ onClick, styleType, text, disabled }) => {
   return (
-    <button onClick={onClick} className={`button ${styleType}`}>
+    <button
+      onClick={onClick}
+      className={`button ${styleType}`}
+      disabled={disabled}
+    >
       {text}
     </button>
   );
