7057c0f49eebb9d6373aeb4961355cd2036d34cb
robin <stahl.ro@icloud.com>
2024-06-24 21:50:37 +0200

    finalise frontend

--- packages/frontend/app/src/app/views/DiagnosisHelper/DiagnosisHelper.tsx
+++ packages/frontend/app/src/app/views/DiagnosisHelper/DiagnosisHelper.tsx
@@ -1,74 +1,87 @@
-import React, { useState } from "react";
-import SpeechToText from "../../components/SpeechToText/SpeechToText";
-import Tabs from "../../components/Tabs/Tabs";
-import TextInput from "../../components/TextInput/TextInput";
-import Button from "../../components/Button/Button";
+import React, { useState } from 'react';
+import SpeechToText from '../../components/SpeechToText/SpeechToText';
+import Tabs from '../../components/Tabs/Tabs';
+import TextInput from '../../components/TextInput/TextInput';
+import Button from '../../components/Button/Button';
+import './DiagnosisHelper.css';
 
 const DiagnosisHelper = () => {
-  const [diagnosis, setDiagnosis] = useState("");
+  const [diagnosis, setDiagnosis] = useState('');
   const handleSpeechToText = (transcript: string) => {
     setDiagnosis(transcript);
   };
   const handleDiagnosisRequest = () => {
-    console.log("Diagnose anfordern");
+    console.log('Diagnose anfordern');
   };
   const tabs = [
     {
-      label: "Huggingface v.1",
+      label: 'Huggingface v.1',
       content: (
-        <div className="diagnosis-helper">
-          <i className="fa-solid fa-stethoscope"></i>
+        <>
           <span>
             Bitte geben Sie hier die Symptomatik und Patientendaten an
           </span>
           <SpeechToText onSpeechToText={handleSpeechToText} />
-          <TextInput
-            value={diagnosis}
-            onChange={setDiagnosis}
-            height="100%"
-            width="100%"
-          />
-          <div className="button">
-            <Button
-              onClick={handleDiagnosisRequest}
-              styleType={"primary"}
-              text={"Diagnose anfordern"}
+          <div className="diagnosis-helper__model-tabs__text-input">
+            <TextInput
+              value={diagnosis}
+              onChange={setDiagnosis}
+              height="70vh"
+              width="100%"
             />
           </div>
-        </div>
+          <div className="diagnosis-helper__model-tabs__button">
+            <Button
+              onClick={handleDiagnosisRequest}
+              styleType={'primary'}
+              text={'Diagnose anfordern'}
+            />
+          </div>
+        </>
       ),
     },
     {
-      label: "Huggingface v.2",
+      label: 'Huggingface v.2',
       content: (
-        <div className="diagnosis-helper">
-          <i className="fa-solid fa-stethoscope"></i>
+        <>
           <span>
             Bitte geben Sie hier die Symptomatik und Patientendaten an
           </span>
           <SpeechToText onSpeechToText={handleSpeechToText} />
-          <div className="text-input">
-            <TextInput value={diagnosis} onChange={setDiagnosis} />
-          </div>
-          <div className="button">
-            <Button
-              onClick={handleDiagnosisRequest}
-              styleType={"primary"}
-              text={"Diagnose anfordern"}
+          <div className="diagnosis-helper__model-tabs__text-input">
+            <TextInput
+              value={diagnosis}
+              onChange={setDiagnosis}
+              height="70vh"
+              width="100%"
             />
           </div>
-        </div>
+          <div className="diagnosis-helper__model-tabs__button">
+            <Button
+              onClick={handleDiagnosisRequest}
+              styleType={'primary'}
+              text={'Diagnose anfordern'}
+            />
+          </div>
+        </>
       ),
     },
     {
-      label: "Feedback",
+      label: 'Feedback',
       content: <div>Bitte gib hier dein Feedback...</div>,
     },
   ];
   return (
-    <>
-      <Tabs tabs={tabs} />
-    </>
+    <div className="diagnosis-helper">
+      <div className="diagnosis-helper__model-tabs">
+        <div className="diagnosis-helper__model-tabs__left">
+          {/* Hier können zusätzliche Inhalte für die linke Seite eingefügt werden */}
+        </div>
+        <div className="diagnosis-helper__model-tabs__right">
+          <Tabs tabs={tabs} />
+        </div>
+      </div>
+    </div>
   );
 };
 export default DiagnosisHelper;
