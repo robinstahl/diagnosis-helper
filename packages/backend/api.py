ae0b7420a79ddbadc03286c4b46b211bbd65e595
Dennis Schumacher <dennis.schumacher@echte-berater.de>
2024-07-04 19:10:56 +0200

    delete databse stuff from apppy

--- packages/backend/api.py
+++ packages/backend/api.py
@@ -17,44 +17,7 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://diagnosis_user:password@localhost:5432/diagnosis_helper'
 
-db = SQLAlchemy(app)
-
-class Request(db.Model):
-    __tablename__ = 'requests'
-
-    id = db.Column(db.Integer, primary_key=True)
-    input_text = db.Column(db.String(255), nullable=False)
-    missed_tokens = db.Column('missedTokens', ARRAY(db.String), nullable=True)
-    wrong_tokens = db.Column('wrongToken', ARRAY(db.String), nullable=True)
-    generatedResponse = db.Column(db.Text, nullable=True)
-    model = db.Column(db.String, nullable=True)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Request {self.input_text}>'
-
-def init_db(app):
-    with app.app_context():
-        db.create_all()
-
-def add_instance(input_text, generated_response_in, model_name):
-    generated_response_str = json.dumps(generated_response_in)
-    new_entry = Request(
-        input_text=input_text,
-        generatedResponse=generated_response_str,
-        model=model_name,
-        timestamp=datetime.now().astimezone()
-    )
-    db.session.add(new_entry)
-    db.session.commit()
-    return new_entry.id
-
-def get_instance_by_id(model, id):
-    return model.query.get(id)
-
-def get_all_instances(model):
-    return model.query.all()
-
+# db = SQLAlchemy(app)
 init_db(app)
 
 @app.route('/test')
